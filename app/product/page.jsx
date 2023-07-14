bafb48bd761fe17128c31937a624cf0a988e184a
bacnd6 <bacnd6@fpt.com>
2023-07-14 13:52:13 +0700

    sale

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -1,8 +1,7 @@
 'use client';
 import useNotification from "@/utils/hooks/useNotification"
 import { assets } from "@/utils/constants/logo";
-import { NavBar } from "../components";
+
 export default function Page() {
-    <NavBar isAuthenticate={authenticate} />
     
 }
